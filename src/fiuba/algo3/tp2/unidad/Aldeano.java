2a00fde396b27e996c5ce1570c1712bbd298b82b
Andrea Gimenez <gim.andr26@gmail.com>
2018-11-13 14:46:28 -0300

    Pequeño refactor en el método construir cuartel
    igualmente deje comentarizado lo que estaba antes

--- src/fiuba/algo3/tp2/unidad/Aldeano.java
+++ src/fiuba/algo3/tp2/unidad/Aldeano.java
@@ -6,6 +6,9 @@ import fiuba.algo3.tp2.mapa.CeldaInexistenteException;
 import fiuba.algo3.tp2.mapa.CeldaOcupadaException;
 import fiuba.algo3.tp2.mapa.Mapa;
 import fiuba.algo3.tp2.mapa.Posicion;
+import fiuba.algo3.tp2.movimiento.Direccion;
+import fiuba.algo3.tp2.movimiento.DireccionArriba;
+import fiuba.algo3.tp2.movimiento.DireccionDerecha;
 
 public class Aldeano extends Unidad {
 	
@@ -17,14 +20,16 @@ public class Aldeano extends Unidad {
 
 	    try {
 
-            Posicion derecha = new Posicion(1, 0);
-
+           /* Posicion derecha = new Posicion(1, 0);*/
+	    	Direccion derecha = new DireccionDerecha();
+	    	
             Cuartel cuartel = new Cuartel(super.obtenerPosicion().sumar(derecha), super.obtenerMapa());
-
+            
         } catch (Exception e) {
-
-            Posicion arriba = new Posicion(0, -1);
-
+        	
+        	/* Posicion arriba = new Posicion(0, -1);*/
+        	Direccion arriba = new DireccionArriba();
+        	
             Cuartel cuartel = new Cuartel(super.obtenerPosicion().sumar(arriba), super.obtenerMapa());
 
         }
