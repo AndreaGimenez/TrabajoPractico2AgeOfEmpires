1a405def36917433cfecca59ec49b10078c9129d
Chris <chrisxfs@gmail.com>
2018-11-23 13:38:12 -0300

    fix Entrega_1 Test02

--- test/fiuba/algo3/tp2/integracion/entrega_1/Test02.java
+++ test/fiuba/algo3/tp2/integracion/entrega_1/Test02.java
@@ -1,35 +1,27 @@
 package fiuba.algo3.tp2.integracion.entrega_1;
 
-import Construccion.EdificioNoSoportadoException;
-import fiuba.algo3.tp2.edificio.*;
-import fiuba.algo3.tp2.juego.Jugador;
-import fiuba.algo3.tp2.mapa.*;
-import fiuba.algo3.tp2.reparacion.EdificioConReparadorAsignadoException;
-import fiuba.algo3.tp2.reparacion.EdificioNoAptoParaReparacionException;
-import fiuba.algo3.tp2.turno.Turno;
-import fiuba.algo3.tp2.unidad.Aldeano;
-import fiuba.algo3.tp2.unidad.Unidad;
-import fiuba.algo3.tp2.unidad.UnidadConstants;
-import org.junit.Rule;
-import org.junit.Test;
-import org.junit.rules.ExpectedException;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
 import java.util.LinkedList;
 
-import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
-
-import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
-
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 
+import Construccion.EdificioNoSoportadoException;
+import fiuba.algo3.tp2.edificio.Edificio;
+import fiuba.algo3.tp2.edificio.EdificioConstants;
+import fiuba.algo3.tp2.edificio.EdificioEnConstruccionException;
+import fiuba.algo3.tp2.edificio.EdifioNoAptoParaContruirException;
+import fiuba.algo3.tp2.edificio.GestionarConstruccion;
+import fiuba.algo3.tp2.edificio.PosicionarEdificio;
+import fiuba.algo3.tp2.edificio.UnidadNoSoportadaException;
 import fiuba.algo3.tp2.mapa.CeldaInexistenteException;
 import fiuba.algo3.tp2.mapa.CeldaOcupadaException;
 import fiuba.algo3.tp2.mapa.Mapa;
 import fiuba.algo3.tp2.mapa.Posicion;
+import fiuba.algo3.tp2.mapa.Posicionable;
 import fiuba.algo3.tp2.mapa.TamanioInvalidoException;
 import fiuba.algo3.tp2.movimiento.DireccionAbajoDerecha;
 import fiuba.algo3.tp2.movimiento.DireccionAbajoIzquierda;
@@ -41,11 +33,14 @@ import fiuba.algo3.tp2.movimiento.DireccionIzquierda;
 import fiuba.algo3.tp2.movimiento.MovimientoInvalidoException;
 import fiuba.algo3.tp2.reparacion.EdificioConReparadorAsignadoException;
 import fiuba.algo3.tp2.reparacion.EdificioNoAptoParaReparacionException;
+import fiuba.algo3.tp2.turno.Turno;
 import fiuba.algo3.tp2.unidad.Aldeano;
 import fiuba.algo3.tp2.unidad.ArmaAsedio;
 import fiuba.algo3.tp2.unidad.Arquero;
 import fiuba.algo3.tp2.unidad.DireccionAbajo;
 import fiuba.algo3.tp2.unidad.Espadachin;
+import fiuba.algo3.tp2.unidad.Unidad;
+import fiuba.algo3.tp2.unidad.UnidadConstants;
 
 /**
  * Pruebas de Unidades
@@ -508,10 +503,6 @@ public class Test02 {
     verificar que se haga en los turnos propios al jugador
     verificar que no suma oro
     */
-
-    @Rule
-    public ExpectedException exceptionRule = ExpectedException.none();
-
 
     @Test
     public void testVerificarConstruccionDePlazaCentral() throws CeldaOcupadaException, CeldaInexistenteException, TamanioInvalidoException, EdificioNoSoportadoException, EdifioNoAptoParaContruirException, UnidadNoSoportadaException, EdificioConReparadorAsignadoException, EdificioNoAptoParaReparacionException {
