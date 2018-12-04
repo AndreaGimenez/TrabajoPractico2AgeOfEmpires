677802f2a9c9ae795640b17f11d9466230ce3efc
Nachito <ignacio5garcia@hotmail.com>
2018-12-03 22:44:36 -0300

    prueba

--- src/fiuba/algo3/tp2/vista/VistaPosicionable.java
+++ src/fiuba/algo3/tp2/vista/VistaPosicionable.java
@@ -28,142 +28,101 @@ public class VistaPosicionable {
 	private VistaCuartel vistaCuartel;
 	private VistaPlazaCentral vistaPlazaCentral;
 	private VistaCastillo vistaCastillo;
-	private VistaCeldaSinOcupar vistaCeldaSinOcupar;
-	
-	
+
+
 	public VistaPosicionable() {
-		
+
 		super();
-		this.vistaAldeano = new VistaAldeano(this);
+		this.vistaAldeano = new VistaAldeano();
 		this.vistaEspadachin = new VistaEspadachin();
 		this.vistaArquero = new VistaArquero();
 		this.vistaArmaAsedio = new VistaArmaAsedio();
 		this.vistaCuartel = new VistaCuartel();
 		this.vistaPlazaCentral = new VistaPlazaCentral();
 		this.vistaCastillo = new VistaCastillo();
-		this.vistaCeldaSinOcupar = new VistaCeldaSinOcupar();
 	}
-	
+
 	public void dibujarPosicionables() {
-		
+
         for(Node nodo : contenedorMapa.getChildren()) {
 			if(nodo instanceof Pane) {
-				
+
 				Pane pane = (Pane) nodo;
 				int colIndex = contenedorMapa.obtenerColumnIndex(pane);
 				int rowIndex = contenedorMapa.obtenerRowIndex(pane);
 				Celda celda = mapa.obtenerCelda(new Posicion(colIndex, rowIndex));
 				Posicionable posicionable = celda.obtenerPosicionable();
-				
+
 				dibujarPosicionable(posicionable, pane);
 			}
         }
 	}
 
 	public void dibujarPosicionable(Posicionable posicionable, Pane pane) {
-		
-		if(posicionable != null) {
-			
-    		if(posicionable instanceof Aldeano) {
-    			
-    			vistaAldeano.dibujar((Aldeano)posicionable, pane);
-    	        
-    		} else if(posicionable instanceof Espadachin) {
-    			
-    			vistaEspadachin.dibujar((Espadachin)posicionable, pane);
-    			
-    		} else if(posicionable instanceof Arquero) {
-    			
-    			vistaArquero.dibujar((Arquero)posicionable, pane);
-    			
-    		} else if(posicionable instanceof ArmaAsedio) {
-    			
-    			vistaArmaAsedio.dibujar((ArmaAsedio)posicionable, pane);
-    			
-    		} else if(posicionable instanceof Cuartel) {
-    			
-    			vistaCuartel.dibujar((Cuartel)posicionable, pane);
-    			
-    		} else if(posicionable instanceof PlazaCentral) {
-    			
-    			vistaPlazaCentral.dibujar((PlazaCentral)posicionable, pane);
-    			
-    		} else if(posicionable instanceof Castillo) {
-    			
-    			vistaCastillo.dibujar((Castillo)posicionable, pane);
-    			
-    		}
-		}
-	}
 
-	public void dibujarPosicionable(Posicionable posicionable, Posicion posicionAnterior) {
-		
 		if(posicionable != null) {
-			
+
     		if(posicionable instanceof Aldeano) {
-    			
-    			vistaAldeano.dibujar((Aldeano)posicionable, posicionAnterior);
-    	        
+
+    			vistaAldeano.dibujar((Aldeano)posicionable, pane);
+
     		} else if(posicionable instanceof Espadachin) {
-    			
-    			vistaEspadachin.dibujar((Espadachin)posicionable, posicionAnterior);
-    			
+
+    			vistaEspadachin.dibujar((Espadachin)posicionable, pane);
+
     		} else if(posicionable instanceof Arquero) {
-    			
-    			vistaArquero.dibujar((Arquero)posicionable, posicionAnterior);
-    			
+
+    			vistaArquero.dibujar((Arquero)posicionable, pane);
+
     		} else if(posicionable instanceof ArmaAsedio) {
-    			
-    			vistaArmaAsedio.dibujar((ArmaAsedio)posicionable, posicionAnterior);
-    			
+
+    			vistaArmaAsedio.dibujar((ArmaAsedio)posicionable, pane);
+
     		} else if(posicionable instanceof Cuartel) {
-    			
-    			vistaCuartel.dibujar((Cuartel)posicionable, posicionAnterior);
-    			
+
+    			vistaCuartel.dibujar((Cuartel)posicionable, pane);
+
     		} else if(posicionable instanceof PlazaCentral) {
-    			
-    			vistaPlazaCentral.dibujar((PlazaCentral)posicionable, posicionAnterior);
-    			
+
+    			vistaPlazaCentral.dibujar((PlazaCentral)posicionable, pane);
+
     		} else if(posicionable instanceof Castillo) {
-    			
-    			vistaCastillo.dibujar((Castillo)posicionable, posicionAnterior);
-    			
+
+    			vistaCastillo.dibujar((Castillo)posicionable, pane);
+
     		}
 		}
 	}
 
 	public void dibujarControles(Pane nodo) {
-		
+
 		int colIndex = contenedorMapa.obtenerColumnIndex(nodo);
 		int rowIndex = contenedorMapa.obtenerRowIndex(nodo);
-		
+
 		Posicionable posicionable = mapa.obtenerPosicionable(new Posicion(colIndex, rowIndex));
-		contenedorControles.clean();
-		
+
 		if(posicionable != null) {
-			if(posicionable instanceof Aldeano) {
+    		if(posicionable instanceof Aldeano) {
     			vistaAldeano.dibujarControles((Aldeano)posicionable);
     		} else if(posicionable instanceof Castillo){
     			vistaCastillo.dibujarControles((Castillo)posicionable, mapa);
 			} else if(posicionable instanceof Cuartel) {
 				vistaCuartel.dibujarControles((Cuartel) posicionable, mapa);
 			} else if(posicionable instanceof PlazaCentral){
-    			vistaPlazaCentral.dibujarControles((PlazaCentral) posicionable, mapa);
+    			vistaPlazaCentral.dibujarControles((PlazaCentral) posicionable);
 			} else if(posicionable instanceof Arquero) {
 				vistaArquero.dibujarControles((Arquero) posicionable);
 			} else if(posicionable instanceof Espadachin){
     			vistaEspadachin.dibujarControles((Espadachin) posicionable);
 			} else if(posicionable instanceof ArmaAsedio){
     			vistaArmaAsedio.dibujarControles((ArmaAsedio) posicionable);
-			} else
-				vistaCeldaSinOcupar.dibujarControles();
+			}
 		}
 	}
 
 	public void setContenedorControles(ContenedorControles contenedorControles) {
-		
 		this.contenedorControles = contenedorControles;
-		
+
 		this.vistaAldeano.setContenedorControles(contenedorControles);
 
 		this.vistaArmaAsedio.setContenedorControles(contenedorControles);
@@ -177,26 +136,13 @@ public class VistaPosicionable {
 		this.vistaCastillo.setContenedorControles(contenedorControles);
 
 		this.vistaCuartel.setContenedorControles(contenedorControles);
+	}
 
-		this.vistaCeldaSinOcupar.setContenedorControles(contenedorControles);
-	}
-	
 	public void setContenedorMapa(ContenedorMapa contenedorMapa) {
-		
 		this.contenedorMapa = contenedorMapa;
-		this.vistaAldeano.setContenedorMapa(contenedorMapa);
 	}
-	
-	public void setVistaSeleccionador(VistaSeleccionador vistaSeleccionador) {
-		
-		this.vistaAldeano.setVistaSeleccionador(vistaSeleccionador);
-	}
-	
+
 	public void setMapa(Mapa mapa) {
 		this.mapa = mapa;
-	}
-
-	public void limpiarControles() {
-		contenedorControles.clean();
 	}
 }
